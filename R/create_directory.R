711b168ce25e8a6f413a3f14bce5349ac3f2af0e
Hendriico <hendriicomerila@gmail.com>
2020-03-04 16:54:19 +0000

    Created create directory function which creates the underlying folders for using the MOSLR performance functions to improve usability.

---
+++ R/create_directory.R
@@ -0,0 +1,67 @@
+#' Create Directory
+#'
+#' This function creates the required directory for
+#' running the MOSLR performance package.
+#'
+#' @param my.dir character
+#' @param projectname character
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+create_directory <- function(
+  my.dir = choose.dir(),
+  foldername = "PerformanceMonitoring",
+  projectname = "Project"
+
+) {
+
+
+
+# List of folders to be checked and created in the directory --------------
+
+
+  folder_list <- c(
+    paste0(my.dir, "/", foldername),
+    paste0(my.dir, "/", foldername, "/data"),
+    paste0(my.dir, "/", foldername, "/Data/inputs"),
+    paste0(my.dir, "/", foldername, "/Data/outputs"),
+    paste0(my.dir, "/", foldername, "/Data/rdata"),
+    paste0(my.dir, "/", foldername, "/Data/tracking"),
+    paste0(my.dir, "/", foldername, "/Data/tracking/mps"),
+    paste0(my.dir, "/", foldername, "/Data/tracking/ops"),
+    paste0(my.dir, "/", foldername, "/MonthlyPerfReport"),
+    paste0(my.dir, "/", foldername, "/PfmReports")
+    )
+
+
+
+
+# Checking and creating folder structure ----------------------------------
+
+
+  for(folder in folder_list){
+    if(!dir.exists(folder)){
+      dir.create(folder)
+    }
+  }
+
+
+# Creating a project in the directory, if necessary -----------------------
+
+
+  path <- file.path(my.dir, paste0(foldername, "/", projectname, ".Rproj"))
+
+  template_path <- system.file("templates/template.Rproj",
+                               package = "usethis")
+
+  if(!file.exists(path)){
+    file.copy(template_path, path)
+  }
+
+
+}
+
+
