e6a8e706b1687080fb0215d79babbb7579c8f540
Luke Austin <lukeaustin@gmail.com>
2021-04-16 22:02:46 +0100

    Updated render function and RMD file for LUMs and Vacancy report. Mainly formatting and aescetic but also moving towards direct querying of database to update underlying files.

--- R/render_MPOP_report.R
+++ R/render_MPOP_report.R
@@ -1,5 +1,4 @@
 
-
 #' Render MPOP Report
 #'
 #' This functions prepares and loads the required
@@ -23,115 +22,112 @@ render_MPOP_report <- function(
   rmd.file = "MPOP.Rmd",
   my.dir = getwd(),
   load.data = TRUE,
-  dir.output = paste0(my.dir, "/MPOP"),
-  file.name =
-    paste0("MPOPReport-", as.character(format(Sys.Date(), "%Y-%m")), ".pdf"),
+  dir.output = paste0(my.dir, "/LUMsVacancyReport"),
+  file.name = paste0("LUMsVacancyReport-", as.character(format(Sys.Date(), "%Y-%m")), ".pdf"),
   data.period = Sys.Date() %m-% months(1),
   ...
-) {
+  )
+  {
 
-
-# Setup -------------------------------------------------------------------
-
+  # Setup -------------------------------------------------------------------
 
   my_dir <- my.dir
   data.period <- as.Date(data.period)
   lubridate::day(data.period) <- 1
 
   if (load.data) {
-
-    tp_details <- utils::read.csv(paste0(my.dir, "/data/inputs/tp_details.csv"))%>%
+    tp_details <-
+      readr::read_csv(paste0(my_dir, "/data/inputs/tp_details.csv")) %>%
       dplyr::mutate(
         TradingPartyName = stringr::str_replace_all(TradingPartyName, "&", "And"),
-        ShortName = stringr::str_replace_all(ShortName, "&", "And"))
+        ShortName = stringr::str_replace_all(ShortName, "&", "And")
+      )
 
-    longunread <- utils::read.csv(paste0(my.dir, "/data/inputs/longunread.csv"))%>%
-      dplyr::mutate(Period = as.Date(paste0(Period, "-01"), "%Y-%m-%d")) %>%
-      dplyr::group_by(WholesalerID, Period)%>%
-      dplyr::mutate(whole_meters = sum(Total_Meters))%>%
-      dplyr::ungroup()%>%
-      dplyr::group_by(RetailerID, Period)%>%
-      dplyr::mutate(ret_meters = sum(Total_Meters))%>%
-      dplyr::ungroup()%>%
-      dplyr::filter(whole_meters > 100, ret_meters > 100)%>%
-      dplyr::left_join(
-        tp_details, by = c("RetailerID" = "Trading.Party.ID"))%>%
-      dplyr::rename(
-        Retailer = TradingPartyName, ShortRet = ShortName
-        ) %>%
-      dplyr::left_join(
-        tp_details, by = c("WholesalerID" = "Trading.Party.ID"))%>%
-      dplyr::rename(
-        Wholesaler = TradingPartyName, ShortWhole = ShortName
-      )%>%
+    longunread <-
+      readr::read_csv(paste0(my_dir, "/data/inputs/longunread.csv")) %>%
+      #dplyr::mutate(Period = as.Date(paste0(Period, "-01"), "%Y-%m-%d")) %>%
+      dplyr::group_by(WholesalerID, Period) %>%
+      dplyr::mutate(whole_meters = sum(TotalMeters)) %>%
+      dplyr::ungroup() %>%
+      dplyr::group_by(RetailerID, Period) %>%
+      dplyr::mutate(ret_meters = sum(TotalMeters)) %>%
+      dplyr::ungroup() %>%
+      dplyr::filter(whole_meters > 100, ret_meters > 100) %>%
+      dplyr::left_join(tp_details, by = c("RetailerID" = "Trading.Party.ID")) %>%
+      dplyr::rename(Retailer = TradingPartyName, ShortRet = ShortName) %>%
+      dplyr::left_join(tp_details, by = c("WholesalerID" = "Trading.Party.ID")) %>%
+      dplyr::rename(Wholesaler = TradingPartyName, ShortWhole = ShortName) %>%
       dplyr::mutate(Pair = paste(ShortRet, "+", ShortWhole))
 
 
-    vacant <- utils::read.csv(paste0(my.dir, "/data/inputs/Vacancy.csv"))%>%
-      dplyr::group_by(WholesalerID, Period)%>%
-      dplyr::mutate(whole_vacant = sum(Premises))%>%
-      dplyr::ungroup()%>%
-      dplyr::group_by(RetailerID, Period)%>%
-      dplyr::mutate(ret_vacant = sum(Premises))%>%
-      dplyr::ungroup()%>%
+    vacant <-
+      readr::read_csv(paste0(my_dir, "/data/inputs/vacancy.csv")) %>%
+      dplyr::group_by(WholesalerID, Period) %>%
+      dplyr::mutate(whole_vacant = sum(Premises)) %>%
+      dplyr::ungroup() %>%
+      dplyr::group_by(RetailerID, Period) %>%
+      dplyr::mutate(ret_vacant = sum(Premises)) %>%
+      dplyr::ungroup() %>%
       dplyr::filter(
         whole_vacant > 200,
         ret_vacant > 200
-      )%>%
-      dplyr::mutate(
-        Period = as.Date(Period, tryFormats = c("%d/%m/%Y", "%Y-%m-%d"))
-      )%>%
-      dplyr::left_join(
-        tp_details,
-        by = c("RetailerID" = "Trading.Party.ID"))%>%
+        ) %>%
+      #dplyr::mutate(Period = as.Date(Period, tryFormats = c("%d/%m/%Y", "%Y-%m-%d"))) %>%
+      dplyr::left_join(tp_details, by = c("RetailerID" = "Trading.Party.ID")) %>%
       dplyr::rename(
         Retailer = TradingPartyName,
         ShortRet = ShortName
-      )%>%
-      dplyr::left_join(
-        tp_details,
-        by = c("WholesalerID" = "Trading.Party.ID"))%>%
+        ) %>%
+      dplyr::left_join(tp_details, by = c("WholesalerID" = "Trading.Party.ID")) %>%
       dplyr::rename(
         Wholesaler = TradingPartyName,
         ShortWhole = ShortName
-      )%>%
+        ) %>%
       dplyr::mutate(
         Pair = stringr::str_replace_all(paste(ShortRet, "+", ShortWhole), "&", "And"),
         Retailer = stringr::str_replace_all(Retailer, "&", "And"),
-        Wholesaler = stringr::str_replace_all(Wholesaler, "&", "And"))
-
-    vacant_TP <- dplyr::bind_rows(
-      read.csv(paste0(my.dir, "/data/inputs/Vacancy.csv"))%>%
-        dplyr::group_by(RetailerID, Period)%>%
-        dplyr::summarise(Premises = sum(Premises), Vacant_Premises = sum(Vacancies))%>%
-        dplyr::rename(TradingPartyID = RetailerID),
-      read.csv(paste0(my.dir, "/data/inputs/Vacancy.csv"))%>%
-        dplyr::group_by(WholesalerID, Period)%>%
-        dplyr::summarise(Premises = sum(Premises), Vacant_Premises = sum(Vacancies))%>%
-        dplyr::rename(TradingPartyID = WholesalerID)
-    )%>%
-      dplyr::mutate(
-        percent = paste(round(100*(Vacant_Premises)/(Premises),1),"%"),
-        Period = as.Date(Period, tryFormats = c("%d/%m/%Y", "%Y-%m-%d")))%>%
-      dplyr::left_join(
-        ., tp_details,
-        by = c("TradingPartyID" = "Trading.Party.ID")
+        Wholesaler = stringr::str_replace_all(Wholesaler, "&", "And")
       )
 
+    vacant_TP <- dplyr::bind_rows(
+      read.csv(paste0(my_dir, "/data/inputs/vacancy.csv")) %>%
+        dplyr::group_by(RetailerID, Period) %>%
+        dplyr::summarise(
+          Premises = sum(Premises),
+          Vacant_Premises = sum(VacantPremises)
+          ) %>%
+        dplyr::rename(TradingPartyID = RetailerID),
+      readr::read_csv(paste0(my_dir, "/data/inputs/vacancy.csv")) %>%
+        dplyr::group_by(WholesalerID, Period) %>%
+        dplyr::summarise(
+          Premises = sum(Premises),
+          Vacant_Premises = sum(VacantPremises)
+        ) %>%
+        dplyr::rename(TradingPartyID = WholesalerID)
+      ) %>%
+      dplyr::mutate(
+        percent = paste(round(100 * (Vacant_Premises) / (Premises), 1), "%"),
+        Period = as.Date(Period, tryFormats = c("%d/%m/%Y", "%Y-%m-%d"))) %>%
+      dplyr::left_join(tp_details, by = c("TradingPartyID" = "Trading.Party.ID"))
 
-
-    vacant_Total <- read.csv(paste0(my.dir, "/data/inputs/Vacancy.csv"))%>%
-      dplyr::group_by(Period)%>%
-      dplyr::summarise(Total_Premises = sum(Premises), Total_Vacant_Premises = sum(Vacancies))%>%
-      dplyr::mutate(Period = as.Date(Period, tryFormats = c("%d/%m/%Y", "%Y-%m-%d")), Market_Vacancy_Rate = Total_Vacant_Premises/Total_Premises)
-
-
+    vacant_Total <-
+      readr::read_csv(paste0(my_dir, "/data/inputs/vacancy.csv")) %>%
+      dplyr::group_by(Period) %>%
+      dplyr::summarise(
+        Total_Premises = sum(Premises),
+        Total_Vacant_Premises = sum(VacantPremises)
+      ) %>%
+      dplyr::mutate(
+        #Period = as.Date(Period, tryFormats = c("%d/%m/%Y", "%Y-%m-%d")),
+        Market_Vacancy_Rate = Total_Vacant_Premises / Total_Premises
+      )
 
   }
 
 
-# Render MPOP report ------------------------------------------------------
-  if(!dir.exists(dir.output)) {
+  # Render MPOP report ------------------------------------------------------
+
+  if (!dir.exists(dir.output)) {
     dir.create(dir.output)
   }
 
